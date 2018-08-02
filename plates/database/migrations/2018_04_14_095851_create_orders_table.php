35126b6371cdfda4f4f24d97d19c775c44231681
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-08-02 13:09:13 +1000

    update orders table

---
+++ plates/database/migrations/2018_04_14_095851_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('orderid');
+            $table->string('description');
+            $table->integer('tableno');
+            $table->string('timeofarrive');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
